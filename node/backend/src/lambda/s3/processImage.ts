f302b594b537c67701b740584f2ba954c810503a
Jieqing Shi <Jieqing.Shi@bmwgroup.com>
2022-04-21 01:08:44 +0200

    added lambda function for updating image table with processed image url

--- node/backend/src/lambda/s3/processImage.ts
+++ node/backend/src/lambda/s3/processImage.ts
@@ -2,14 +2,16 @@ import { SNSEvent, SNSHandler, S3EventRecord } from 'aws-lambda'
 import 'source-map-support/register'
 import * as AWS from 'aws-sdk'
 import Jimp from 'jimp/es'
+import Axios from 'axios'
 
 const s3 = new AWS.S3()
-const axios = require('axios')
+// const docClient = new AWS.DynamoDB.DocumentClient()
+// const Axios = require('axios')
 
 const imagesBucketName = process.env.IMAGES_S3_BUCKET
-const processedImagesBucketName = process.env.IMAGES_PROCESSED_S3_BUCKET
+// const imagesProcessedTable = process.env.IMAGES_PROCESSED_TABLE
+const imagesProcessedBucketName = process.env.IMAGES_PROCESSED_S3_BUCKET
 const modelEndpoint = process.env.MODEL_ENDPOINT
-
 
 export const handler: SNSHandler = async (event: SNSEvent) => {
     console.log('Processing SNS event ', JSON.stringify(event))
@@ -27,7 +29,7 @@ export const handler: SNSHandler = async (event: SNSEvent) => {
 
 async function getPredictions(imageUrl: string): Promise<any> {
   console.log(`Sending image from URL ${imageUrl} to model endpoint`)
-  const result = await axios.post(modelEndpoint, {
+  const result = await Axios.post(modelEndpoint, {
     "url": imageUrl
   })
   console.log("Predictions obtained from model endpoint")
@@ -37,17 +39,38 @@ async function getPredictions(imageUrl: string): Promise<any> {
   return result.data
 }
 
+async function processImage(record: S3EventRecord) {
+  const key = record.s3.object.key  // Check cloudwatch logs to see how record looks like
+  const imageUrl = `https://${imagesBucketName}.s3.amazonaws.com/${key}`  // construct imageUrl from the key name
+  console.log('Processing S3 item with key: ', key)
+  console.log("The image URL is ", imageUrl)
+
+  // We're getting the image from the URL instead of from S3 like in the lecture
+  // This is because we also need the URL to obtain the prediction results from the model endpoint
+  const image = await resizeAndApplyTextOverlay(imageUrl)
+  
+  const convertedBuffer = await image.getBufferAsync(Jimp.AUTO)
+  
+  console.log(`Saving processed images in S3 Bucket ${imagesProcessedBucketName}`)
+  await s3.putObject({
+        Bucket: imagesProcessedBucketName,
+        // Key: `${key}.jpg`,
+        Key: key,  // gets same key as the original image
+        Body: convertedBuffer
+  }).promise()
+}
+
 async function resizeAndApplyTextOverlay(imageUrl) {
   // Reads image from an URL, resizes it, applies a text overlay and returns the image as a buffer
   // Reading image
   const image = await Jimp.read(imageUrl);
   console.log("Read image. Image has size: ", image.bitmap.width, image.bitmap.height)
-  image.resize(600, Jimp.AUTO);
+  // Resize image
+  image.resize(1000, Jimp.AUTO);
   const width = image.bitmap.width;
   const height = image.bitmap.height;
   console.log("Resized image. Image has size: ", width, height)
-  var textImage = new Jimp(width, height, 0x0);
-
+  
   // Get predictions from model endpoint which serves as the text to be overlayed
   console.log("Getting predictions from model endpoint")
   const predictionResults = await getPredictions(imageUrl)
@@ -55,6 +78,7 @@ async function resizeAndApplyTextOverlay(imageUrl) {
   const probabilities = predictionResults.prob5
 
   // Defining the text font and create the overlay with a custom color
+  var textImage = new Jimp(width, height, 0x0);
   const font = await Jimp.loadFont(Jimp.FONT_SANS_16_BLACK);
   for (let i = 0; i < breeds.length; i++) {
     textImage.print(font, 10, 10 + i * 20, `${breeds[i].split("_").join(" ")}: ${Math.round(probabilities[i]*10000)/100}%`);
@@ -65,38 +89,17 @@ async function resizeAndApplyTextOverlay(imageUrl) {
   return image
 }
 
-async function processImage(record: S3EventRecord) {
-    const key = record.s3.object.key  // Check cloudwatch logs to see how record looks like
-    const imageUrl = `https://${imagesBucketName}.s3.amazonaws.com/${key}`  // construct imageUrl from the key name
-    console.log('Processing S3 item with key: ', key)
-    console.log("The image URL is ", imageUrl)
+// We need to store the processed image Url in dynamodb so that the client can access it. There are multiple ways to do this.
 
-    // get image from S3 (could also get it via a GET request from the imageUrl using axios?)
-    // const response = await s3.getObject({
-    //       Bucket: imagesBucketName,
-    //       Key: key
-    // }).promise()
-    
-  
-    // process image
+// OPTION1: we can create a new dynamodb table that mirrors the image table, with the difference being that the image Url is that of the processed image bucket.
+// To do this we can obtain the imageId (=key) from the S3 event and use it to construct the processed image Url. Then add code here in this function to create a new record in the dynamodb table.
+// (similar to createImage.ts)
 
-    // const body = response.Body
-    // console.log('Resizing image now')
-    
-    // const image = await Jimp.read(body)
-    // image.resize(512, Jimp.AUTO)
-    // textOverlay(image, top5, prob5)
+// OPTION2: add a S3 Event Listener lambda handler to monitor to the processed image bucket and update the image table with the new processed image URL once a new item is being added to the bucket
 
-    // We're getting the image from the URL instead since we also need the URL to obtain the prediction results from the model endpoint
-    const image = await resizeAndApplyTextOverlay(imageUrl)
-    
-    const convertedBuffer = await image.getBufferAsync(Jimp.AUTO)
-    
-    console.log(`Saving processed images in S3 Bucket ${processedImagesBucketName}`)
-    await s3.putObject({
-          Bucket: processedImagesBucketName,
-          Key: `${key}.jpg`,
-          Body: convertedBuffer
-    }).promise()
-}
+// OPTION3: we use the existing image table, add a API Gateway lambda handler for replacing the imageURL/adding a new key for the processed image url.
+// Then call this API Gateway from the frontend (or here via axios)
+
+// Trying option 3 for now
+
 
