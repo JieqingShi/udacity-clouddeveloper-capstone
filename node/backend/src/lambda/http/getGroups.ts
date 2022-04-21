eb556bdfca6a096e3632419bd58218536eff7599
Jieqing Shi <Jieqing.Shi@bmwgroup.com>
2022-04-21 20:03:21 +0200

    Commit after some refactoring

--- node/backend/src/lambda/http/getGroups.ts
+++ node/backend/src/lambda/http/getGroups.ts
@@ -3,26 +3,19 @@ import 'source-map-support/register'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { createLogger } from '../../utils/logger'
-import * as AWS  from 'aws-sdk'
+import { getAllGroups } from '../../businessLogic/groups';
 
-
-const docClient = new AWS.DynamoDB.DocumentClient()
-const groupsTable = process.env.GROUPS_TABLE
 const logger = createLogger('getGroupsLogger')
 
 export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   logger.info('Processing event: ', event)
 
-  const result = await docClient.scan({
-    TableName: groupsTable
-  }).promise()
-
-  const items = result.Items
+  const groups = await getAllGroups()
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      items
+      items: groups
     })
   }
 })
